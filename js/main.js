d39b6da8dd55f5ec97a504c3f057ea01e86044b9
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-17 02:42:55 +0100

    update js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const wrapper = document.querySelector(".wrapper");
-const inputPart = document.querySelector(".input-part");
+const inputPart = wrapper.querySelector(".input-part");
 const infoText = inputPart.querySelector(".info-text");
 const inputField = inputPart.querySelector("input");
 
@@ -15,6 +15,10 @@ inputField.addEventListener("keyup", (e) => {
 function requestApi(city) {
   let apiKey = "07334a881c572f9314ab02c71984919b";
   let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  infoText.innerText = "Getting weather data...";
+  infoText.classList.add("pending");
+  infoText.style.display = "block";
 
   fetch(api).then((response) =>
     console.log(response.json()).then((result) => weatherDetails(result))
