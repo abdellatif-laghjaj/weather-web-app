4fc7886eb2405c9f387ee079456c0da4d0445305
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-17 02:18:41 +0100

    api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-const wrapper = document.querySelector('.wrapper');
-const inputPart = document.querySelector('.input-part');
-const infoText = inputPart.querySelector('.info-text');
+const wrapper = document.querySelector(".wrapper");
+const inputPart = document.querySelector(".input-part");
+const infoText = inputPart.querySelector(".info-text");
+const inputField = inputPart.querySelector("input");
+
+inputField.addEventListener("keyup", (e) => {
+
+    //if user presses enter and input is not empty
+    if (e.key == "Enter" && inputField.value != "") {
+        requestApi(inputField.value);
+        inputField.value = "";
+    }
+});
+
+//request API
+function requestApi(city){
+    let apiKey = "07334a881c572f9314ab02c71984919b";
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    fetch(api).then(response => {
+        console.log(response.json());
+    });
+}
