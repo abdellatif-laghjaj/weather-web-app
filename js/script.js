0c63f8ddff399e98a1e652a708ab748d5bb087ee
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-17 14:55:31 +0100

    change theme script

--- js/script.js
+++ js/script.js
@@ -103,3 +103,51 @@ function weatherDetails(info) {
 arrowBack.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
+
+//change color theme
+//get the theme from local storage
+getTheme();
+
+//color palette
+const colors = [
+  "hsl(345, 80%, 50%)",
+  "hsl(100, 80%, 50%)",
+  "hsl(200, 80%, 50%)",
+  "hsl(227, 66%, 55%)",
+  "hsl(26, 80%, 50%)",
+  "hsl(44, 90%, 51%)",
+  "hsl(280, 100%, 65%)",
+  "hsl(480, 100%, 25%)",
+  "hsl(180, 100%, 25%)",
+];
+
+const colorBtns = document.querySelectorAll(".theme-color");
+
+//loop through colors array and set each color to a button
+for (let i = 0; i < colorBtns.length; i++) {
+  colorBtns[i].style.backgroundColor = colors[i];
+}
+
+colorBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    changeTheme(btn.style.backgroundColor);
+  });
+});
+
+function changeTheme(color) {
+  document.documentElement.style.setProperty("--bg-color", color);
+  saveTheme(color);
+}
+
+//get the theme from local storage
+function getTheme() {
+  const theme = localStorage.getItem("theme");
+  if (theme) {
+    changeTheme(theme);
+  }
+}
+
+//save the theme to local storage
+function saveTheme(color) {
+  localStorage.setItem("theme", color);
+}
